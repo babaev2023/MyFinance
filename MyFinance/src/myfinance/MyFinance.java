eb557d666580614bfab91fed98374692a0c9059f
babaev2023 <stepan.babaev@gmail.com>
2023-06-29 10:42:20 +0300

    settings

--- MyFinance/src/myfinance/MyFinance.java
+++ MyFinance/src/myfinance/MyFinance.java
@@ -4,6 +4,17 @@
  */
 package myfinance;
 
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import myfinance.settings.Text;
+
 /**
  *
  * @author Stepan Babaev
@@ -12,7 +23,22 @@ public class MyFinance {
 
     
     public static void main(String[] args) {
-        System.out.println("myfinance.MyFinance.main()");
+        init();
+        //Проверяем что работает
+        System.out.println(Text.get("PROGRAMM_NAME"));
+        System.out.println(Arrays.toString(Text.getMonths()));
+    }
+    
+    
+    private static void init() {
+        Text.init();
+        GraphicsEnvironment ge =GraphicsEnvironment.getLocalGraphicsEnvironment();
+        try {
+            boolean registerFont;
+            registerFont = ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/AcariSans-Italic.ttf") ));
+                    } catch (FontFormatException | IOException ex) {
+            Logger.getLogger(MyFinance.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
---
+++ MyFinance/src/myfinance/settings/Text.java
@@ -0,0 +1,62 @@
+/*
+ * Text
+ *  AcariSans-Italic
+ */
+package myfinance.settings;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Stepan Babaev
+ */
+final public class Text {
+    
+    private static final HashMap<String, String> data = new HashMap();
+    
+    public static String get(String key) {
+        return data.get(key);
+    }
+    
+    public static String[] getMonths() {
+        String[] months = new String[12];
+        months[0] = get("JANUARY");
+        months[1] = get("FEBRUAR");
+        months[2] = get("MARCH");
+        months[3] = get("APRIL");
+        months[4] = get("MAY");
+        months[5] = get("JUNE");
+        months[6] = get("JULY");
+        months[7] = get("AUGUST");
+        months[8] = get("SEPTEMBER");
+        months[9] = get("OCTOBER");
+        months[10] = get("NOVEMBER");
+        months[11] = get("DECEMBER");
+        return months;
+    }
+    
+    
+    public static void init() {
+        data.put("PROGRAMM_NAME", "Моя Бухгалтерия");
+        data.put("MENU_FILE", "Файл");
+        data.put("MENU_EDIT", "Правка");
+        data.put("MENU_VIEW", "Вид");
+        data.put("MENU_HELP", "Помощь");
+        
+        data.put("JANUARY", "Январь");
+        data.put("FEBRUAR", "Феварль");
+        data.put("MARCH", "Март");
+        data.put("APRIL", "Апрель");
+        data.put("MAY", "Май");
+        data.put("JUNE", "Июнь");
+        data.put("JULY", "Июль");
+        data.put("AUGUST", "Август");
+        data.put("SEPTEMBER", "Сентябрь");
+        data.put("OCTOBER", "Октябрь");
+        data.put("NOVEMBER", "Ноябрь");
+        data.put("DECEMBER", "Декабрь");
+        
+    }
+    
+    
+}
